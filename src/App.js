f39234cdd788dc0ccae91c18c9f5cae0529be0a6
Muh-Hasan <muh.hasan.git@gmail.com>
2020-09-16 02:20:34 -0700

    basicLayout done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import './App.css';
+import {  Route , Routes  } from 'react-router-dom'
+import Login from './components/Login/index'
+import SignUp from './components/Sign up/index'
+import Dashborad from './components/Dashborad/index'
 
 function App() {
   return (
     <div>
+      <Routes>
+        <Route path='/' element={} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<SignUp />} />
+        <Route path='/dashborad' element={<Dashborad />} />
+      </Routes>
     </div>
   );
 }
